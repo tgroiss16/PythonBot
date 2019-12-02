9c3a9bbdf1477f436c4ee92c8409d27e6063a8ba
byFloriiian <florian.eggetsbergerfefe@gmail.com>
2019-12-02 16:34:08 +0100

    csv_export_added vcf_export_half_added

--- ProjectArcher_Backend/Services/ContactService.cs
+++ ProjectArcher_Backend/Services/ContactService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ProjectArcher_Backend.Models;
 
@@ -47,6 +49,62 @@ namespace ProjectArcher_Backend.Services
             var contactToReturn = _context.Contact.Where(contact => contact.Id == id).Single();
             _context.SaveChanges();
             return contactToReturn;
+        }
+
+        public void ContactToCsv(Contact c, string filename)
+        {
+            ContactToCsv(new Contact[] { c }, filename);
+        }
+
+        public void ContactToCsv(Contact[] contacts, string filename)
+        {
+
+            string firstLine = "Vorname;Nachname;Titel Vorgestellt;Titel nachgestellt;Geschlecht;Position in der Firma;Unternehmen;Aktiv/Inaktiv;Telefon Mobil;TelefonFestnetz;email;Kontaktnotizen;Quelle;interner Kontakt";
+            var sContacts = new List<string>();
+            sContacts.Add(firstLine);
+            foreach (Contact contact in contacts)
+            {
+                sContacts.Add(ToCsvString(contact));
+            }
+            File.WriteAllLines(filename, sContacts);
+        }
+
+        private string ToCsvString(Contact contact)
+        {
+            return $"{contact.FirstName};{contact.LastName};{contact.TitlePrefix};{contact.TitlePostfix};{contact.Gender};{contact.Position};{contact.Company.Name};{contact.IsActive};{contact.PhoneNumberMobile};{contact.PhoneNumberLandline};{contact.Email};{contact.Note};{contact.Source};{contact.InternalContact}";
+        }
+
+        public void ContactToVcf(Contact c, string filename)
+        {
+            ContactToVcf(new Contact[] { c }, filename);
+        }
+
+        public void ContactToVcf(Contact[] contacts, string filename)
+        {
+            foreach (var contact in contacts)
+            {
+                var builder = new StringBuilder();
+                builder.AppendLine("BEGIN:VCARD");
+                builder.AppendLine("VERSION:2.1");
+                builder.Append("N:").Append(contact.LastName)
+                  .Append(";").AppendLine(contact.FirstName);
+                builder.Append("FN:").Append(contact.FirstName)
+                  .Append(" ").AppendLine(contact.LastName);
+                builder.Append("ADR;HOME;PREF:;;").Append(contact.Company.Street)
+                  .Append(";").Append(contact.Company.City).Append(";")
+                  .Append(contact.Company.PostalCode).Append(";").AppendLine(contact.Company.Country);
+                builder.Append("ORG:").AppendLine(contact.Company.Name);
+                builder.Append("TITLE:").AppendLine(contact.TitlePrefix);
+                builder.Append("TITLE2:").AppendLine(contact.TitlePostfix);
+                builder.Append("TEL;WORK;VOICE:").AppendLine(contact.PhoneNumberLandline);
+                builder.Append("TEL;CELL;VOICE:").AppendLine(contact.PhoneNumberMobile);
+                builder.Append("URL:").AppendLine(contact.Company.Website);
+                builder.Append("EMAIL;PREF;INTERNET:").AppendLine(contact.Email);
+
+                builder.AppendLine("END:VCARD");
+                string filenamecontact = $@"{filename}_{contact.Company.Name}_{contact.LastName}_{contact.FirstName} .vcf";
+                File.WriteAllText(filenamecontact, builder.ToString());
+            }
         }
     }
 }
