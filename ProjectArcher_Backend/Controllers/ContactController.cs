11c385af7a416ab71409ec365533db79c923943f
aeckerstorfer <andieckerstorfer2002@gmail.com>
2020-01-27 16:24:25 +0100

    Gangls Fehler

--- ProjectArcher_Backend/Controllers/ContactController.cs
+++ ProjectArcher_Backend/Controllers/ContactController.cs
@@ -23,7 +23,9 @@ namespace ProjectArcher_Backend.Controllers
         [HttpGet]
         public ActionResult<List<ContactDTO>> GetAllContacts()
         {
-            return Ok(_contactService.GetContacts().Select(x => ContactDTO.Of(x)));
+            var contacts = _contactService.GetContacts();
+            var dtos = contacts.Select(x => ContactDTO.Of(x)).ToList();
+            return Ok(dtos);
         }
 
         [HttpGet("{id}")]
