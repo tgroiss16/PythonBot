417b48f3189371879ba5347d17d2724a4ba74a60
byFloriiian <florian.eggetsbergerfefe@gmail.com>
2020-02-03 15:59:38 +0100

    Added Company and Contact CSV Added FillDataBase() in Program

--- ProjectArcher_Backend/Program.cs
+++ ProjectArcher_Backend/Program.cs
@@ -7,14 +7,26 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using ProjectArcher_Backend.Models;
+using ProjectArcher_Backend.Services;
 
 namespace ProjectArcher_Backend
 {
     public class Program
     {
+        private static postgresContext db = new postgresContext();
+        private static ContactService c = new ContactService(db);
+        private static CompanyService comp = new CompanyService(db);
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            FillDataBase();
+        }
+
+        private static void FillDataBase()
+        {
+            db.Contact.AddRange(c.ReadContactCSV("Contact.csv"));
+            db.Company.AddRange(comp.ReadCompanyCSV("Companies.csv"));
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
