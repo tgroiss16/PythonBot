f7f4e5fa32ff9d664e063c00efe7d90c2a928737
aeckerstorfer <andieckerstorfer2002@gmail.com>
2020-03-02 09:59:30 +0100

    set up unit testing

---
+++ Unit_Tests/ExampleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Unit_Tests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void Test_Example()
+        {
+            Assert.True("Example" == "Example", "This test is richtig, spezi");
+        }
+    }
+}
